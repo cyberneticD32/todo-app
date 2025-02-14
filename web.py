bddb695e3804c116c150e30ec4655f3e4183fd41
diegoaguilar <tablet.diego.aguilar@gmail.com>
2025-02-14 11:13:38 -0600

    Change theme, add clock, handle edit and complete error

--- web.py
+++ web.py
@@ -7,8 +7,7 @@ st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase you productivity")
 
-
 for todo in todos:
-    st.checkbox(todos)
+    st.checkbox(todo)
 
-st.text_input(label="Enter a todo: ")
+st.text_input(label="Enter a todo: ", placeholder="Add new todo...")
